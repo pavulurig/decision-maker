6a7b0dc0f981b5feca0d9ed12cfde7087d3c9b8d
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-09-23 19:45:56 -0500

    Pusing Build Folder

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IndecisionApp from './components/Indecision'
-import 'normalize-css/normalize.css';
-import './style/style.scss'
-ReactDOM.render(<IndecisionApp/>,document.getElementById("app"))
